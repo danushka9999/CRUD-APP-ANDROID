8f7489adccff0078c06b962d2d4d859e2ffec5dc
danushka9999 <dranasinghe99@gmail.com>
2021-10-28 14:20:16 +0530

    save user front completed

--- app/src/main/java/com/example/datahandling/MainActivity.java
+++ app/src/main/java/com/example/datahandling/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.datahandling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.datahandling.database.DBHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText editTextTextPersonName;
+    EditText editTextTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+
     }
+
+    public void saveUser(View view){
+        String name = editTextTextPersonName.getText().toString();
+        String password = editTextTextPersonName.getText().toString();
+        DBHelper dbHelper = new DBHelper(this);
+
+        if(name.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Enter values please",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            dbHelper.addInfo(name,password);
+            Toast.makeText(this,"Data Inserted",Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
